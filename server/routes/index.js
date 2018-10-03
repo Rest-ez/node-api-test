c0d9978f4e06c98787f9548ebc39a6c39e1821e9
Eric Kreinar <erickreinar@basecampdev02s-MacBook-Pro.local>
2018-10-03 07:59:24 -0400

    remove todoitems route

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,5 +8,5 @@ module.exports = (app) => {
 
   app.post('/api/todos', todosController.create);
   app.get('/api/todos', todosController.list);
-  app.post('/api/todos/:todoId/items', todoItemsController.create);
+  //app.post('/api/todos/:todoId/items', todoItemsController.create);
 };
