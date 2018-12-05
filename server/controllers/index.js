0eecc0fb672d8ee80bfcb3b738a706fdc4be91ef
Ubuntu <eric.kreinar@basecampcs.com>
2018-12-05 17:07:04 +0000

    commit changes for push to new branch

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,6 @@
-const todos = require('./todos');
 const users = require('./users');
-const todoItems = require('./todoItems');
+
 
 module.exports = {
-  todos,
-  todoItems,
   users
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,10 @@
-const todosController = require('../controllers').todos;
-const todoItemsController = require('../controllers').todoItems;
+const usersController = require('../controllers').users;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/todos', todosController.create);
-  app.get('/api/todos', todosController.list);
   app.post('/api/users', usersController.create);
-  app.get('/api/users', usersController.list);
-  //app.post('/api/todos/:todoId/items', todoItemsController.create);
+	app.get('/api/users', usersController.list);
 };
